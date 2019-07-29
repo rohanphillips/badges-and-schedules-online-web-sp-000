65238f6a3daa9069113e1befacdf11fcc68efe25
Rohan Phillips <rohan@solutions-insurance.com>
2019-07-29 19:08:44 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,3 +6,10 @@ end
 def batch_badge_creator(array)
   array.collect{|n| "Hello, my name is #{n}."}
 end
+
+def assign_rooms(array)
+  newarray = []
+  array.each do |n|
+    newarray << "Hello, #{array[n]}! You'll be assigned to room #{n}!"
+  end
+end
